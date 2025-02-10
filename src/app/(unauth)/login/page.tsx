3c91d082f96bc0ba7d520e974ad00080cb9e0912
SeongYoon <namo4862@gmail.com>
2025-02-10 16:26:52 +0900

    fix

--- src/app/(unauth)/login/page.tsx
+++ src/app/(unauth)/login/page.tsx
@@ -1,16 +1,20 @@
 import { GalleryVerticalEnd } from "lucide-react";
-import { LoginForm } from "../_components/LoginForm";
+
+import LoginForm from "@/components/login-form";
 
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2 w-full">
+      <div className="relative hidden bg-muted lg:block">
+        <div className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale" />
+      </div>
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
             <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
               <GalleryVerticalEnd className="size-4" />
             </div>
-            Acme Inc.
+            탱고플러스 - 센터
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
@@ -19,7 +23,6 @@ export default function LoginPage() {
           </div>
         </div>
       </div>
-      <div className="relative hidden bg-muted lg:block bg-black dark:bg-[#616161]"></div>
     </div>
   );
 }
---
+++ src/app/(unauth)/register/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Register = () => {
+  return <div className="grid min-h-svh lg:grid-cols-2 w-full">Register</div>;
+};
+
+export default Register;
