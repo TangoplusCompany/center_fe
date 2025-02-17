457fe2d7ffe94bb9a8d13df192aee82af7a8c5ac
SeongYoon <namo4862@gmail.com>
2025-02-17 22:06:41 +0900

    feat: Pagination Components 생성

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,42 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
+type PaginationProps = {
+  nowPage: number;
+  setNowPage: (page: number) => void;
+  total: number;
+  limit: number;
+  className?: string;
+};
+
+export function DefaultPagination({ ...props }: PaginationProps) {
+  return (
+    <Pagination className={`${props.className}`}>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" isActive>
+            {props.nowPage}
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+}
