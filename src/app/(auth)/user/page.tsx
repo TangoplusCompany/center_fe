873c35a4d66d75a01eaf12c8723485980584d8ba
SeongYoon <namo4862@gmail.com>
2025-02-17 22:05:38 +0900

    feat: UserPage Components

--- src/app/(auth)/user/page.tsx
+++ src/app/(auth)/user/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { CenterUserList } from "./_components/CenterUserList";
+import UserPage from "./_components/UserPage";
 
 const UserHome = () => {
   return (
     <div className="grid grid-cols-12 gap-5">
       <h1 className="text-2xl col-span-12">센터 사용자 관리</h1>
-      <CenterUserList className="col-span-12" />
+      <UserPage />
     </div>
   );
 };
