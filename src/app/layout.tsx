2a8c8f25683addc914f180b73ef9d26b9b323d93
SeongYoon <namo4862@gmail.com>
2025-02-18 17:52:43 +0900

    feat: MSW 연결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import QueryProvider from "@/providers/QueryProvider";
+import { MSWComponent } from "@/components/MockerComponents";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,7 +36,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <QueryProvider>{children}</QueryProvider>
+          <MSWComponent>
+            <QueryProvider>{children}</QueryProvider>
+          </MSWComponent>
         </ThemeProvider>
       </body>
     </html>
