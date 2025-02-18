06bd19ede2e472a7244064d9f7935bdb248d986e
SeongYoon <namo4862@gmail.com>
2025-02-18 17:51:24 +0900

    fix: config 값 수정

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // compiler: {
-  //   styledComponents: {
-  //     ssr: false,
-  //   },
-  // },
+  
 };
 
 export default nextConfig;
