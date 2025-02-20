2d752f3cdf2d3ec969273edb03888017fb68d7d4
SeongYoon <namo4862@gmail.com>
2025-02-20 17:56:34 +0900

    fix: Login Mocking 추가

--- src/app/(auth)/user/page.tsx
+++ src/app/(auth)/user/page.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import UserPage from "./_components/UserPage";
-import TestComponent from "./_components/TestComponent";
 
 const UserHome = () => {
   return (
     <div className="grid grid-cols-12 gap-5">
       <h1 className="text-2xl col-span-12">센터 사용자 관리</h1>
       <UserPage />
-      <TestComponent />
     </div>
   );
 };
