50f47cd832284fc4257451263175925206b7386a
SeongYoon <namo4862@gmail.com>
2025-02-25 09:00:06 +0900

    fix: react-query retry 추가

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -4,7 +4,13 @@ import React, { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const QueryProvider = ({ children }: { children: ReactNode }) => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 3,
+      },
+    },
+  });
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
