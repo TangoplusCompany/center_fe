dae6790e765923b5d8049f52a79a952e0a16e0c1
SeongYoon <namo4862@gmail.com>
2025-02-27 15:42:45 +0900

    feat: 코치 API 추가

---
+++ src/app/api/coach/route.ts
@@ -0,0 +1,16 @@
+export async function GET() {
+  const data = "Coach Get";
+  return Response.json(data);
+}
+export async function POST() {
+  const data = "Coach Post";
+  return Response.json(data);
+}
+export async function PUT() {
+  const data = "Coach Put";
+  return Response.json(data);
+}
+export async function DELETE() {
+  const data = "Coach Delete";
+  return Response.json(data);
+}
