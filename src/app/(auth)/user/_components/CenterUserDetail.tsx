e685cc42039fbada9f367e3c27abfc7ebb353c78
SeongYoon <namo4862@gmail.com>
2025-03-07 17:48:32 +0900

    feat: 유저 상세 페이지 Mocking데이터를 기반으로 데이터 호출 구현

--- src/app/(auth)/user/_components/CenterUserDetail.tsx
+++ src/app/(auth)/user/_components/CenterUserDetail.tsx
@@ -1,20 +1,30 @@
 "use client";
 
-import { UserData } from "@/types/user";
+import { UserDetail } from "@/types/user";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const CenterUserDetail = ({ sn }: { sn: number }) => {
-  const { data, isLoading } = useQuery<UserData>({
+  const { data, isLoading } = useQuery<UserDetail>({
     queryKey: ["UserMain"],
     queryFn: async () => {
-      const response = await fetch(`/api/user?user_sn=${sn}`, {
+      const response = await fetch(`/api/user/${sn}`, {
         method: "GET",
       });
       return await response.json();
     },
   });
-  return <>{isLoading ? <p>Loading...</p> : <p>{JSON.stringify(data)}</p>}</>;
+  return (
+    <>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="w-full whitespace-pre-wrap">
+          <p>{JSON.stringify(data)}</p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default CenterUserDetail;
