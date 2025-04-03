27d7f7da96ef52d885384b7f2bd5cab87f1db21a
SeongYoon <namo4862@gmail.com>
2025-04-03 16:03:40 +0900

    feat: 유저 추가 페이지 구현중

---
+++ src/app/(auth)/user/_components/UserAddContainer.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React, { ReactNode, useState } from "react";
+import { ICoachData } from "@/types/coach";
+
+const EmptyUserList = () => (
+  <div className="w-full flex items-center justify-center py-10">
+    <ErrorText>조회된 사용자가 없습니다.</ErrorText>
+  </div>
+);
+
+const loginSchema = z.object({
+  name: z.string().min(1, {
+    message: "이름을 입력해주세요.",
+  }),
+});
+
+const ErrorText = ({ children }: { children: ReactNode }) => {
+  return <p className="text-lg font-medium text-red-500">{children}</p>;
+};
+
+const SearchAllCoach = ({
+  updateUser,
+}: {
+  updateUser: (coach: ICoachData) => void;
+}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+  });
+  const [coachList, setCoachList] = useState<ICoachData[]>([]);
+  const SearchCoachHandler = handleSubmit(async (data) => {
+    const result = await fetch(`/api/user/search?name=${data.name}`, {
+      method: "GET",
+    });
+    if (result.ok) {
+      const data = await result.json();
+      setCoachList(data.users);
+    } else {
+      if (result.status === 404) {
+        setCoachList([]);
+      }
+    }
+  });
+  const selectCoachHandler = (id: string) => {
+    const selectedCoach = coachList.find((coach) => coach.id === id);
+    if (selectedCoach) {
+      updateUser(selectedCoach);
+      setCoachList([]);
+    }
+  };
+  return (
+    <article className="w-full">
+      <form className="w-full flex gap-5 mb-5" onSubmit={SearchCoachHandler}>
+        <Input
+          placeholder="이름을 입력해주세요."
+          type="text"
+          {...register("name")}
+        />
+        <Button variant="outline" type="submit">
+          조회하기
+        </Button>
+      </form>
+      <div className="flex flex-col w-full rounded-md border border-input bg-transparent text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm">
+        {coachList.length === 0 ? (
+          <EmptyUserList />
+        ) : (
+          <>
+            <div className="grid grid-cols-6 px-3 py-2 border-b border-solid border-gray-300">
+              <p className="col-span-1 text-center">이름</p>
+              <p className="col-span-1 text-center">이메일</p>
+              <p className="col-span-1 text-center">전화번호</p>
+              <p className="col-span-1" />
+            </div>
+            {coachList.map((coach) => {
+              return (
+                <div
+                  key={coach.id + coach.name}
+                  className="grid grid-cols-6 items-center px-3 py-2 hover:bg-gray-100 border-b last:border-none border-solid border-gray-300"
+                >
+                  <p className="col-span-1 text-center"></p>
+                  <p className="col-span-1 text-center"></p>
+                  <p className="col-span-1 text-center"></p>
+                  <p className="col-span-1 text-center"></p>
+                  <p className="col-span-1 text-center"></p>
+                  <button
+                    type="button"
+                    onClick={() => selectCoachHandler(coach.id)}
+                    className="col-span-1 text-center"
+                  >
+                    선택
+                  </button>
+                </div>
+              );
+            })}
+          </>
+        )}
+      </div>
+    </article>
+  );
+};
+
+const UserAddContainer = () => {
+  const [user, setUser] = useState<ICoachData[] | []>([]);
+  const getUserData = (user: ICoachData) => {
+    setUser((prev) => {
+      if (prev.length === 0) {
+        return [user];
+      } else {
+        const userIndex = prev.findIndex((u) => u.id === user.id);
+        if (userIndex !== -1) {
+          return prev;
+        } else {
+          return [...prev, user];
+        }
+      }
+    });
+  };
+  return (
+    <>
+      {/* 유저 검색 */}
+      <SearchAllCoach updateUser={getUserData} />
+    </>
+  );
+};
+
+export default UserAddContainer;
