ae66fe66d636821e386fc51bb59a8a87ddfe7f44
SeongYoon <namo4862@gmail.com>
2025-04-03 16:03:02 +0900

    feat: 공용상수 추가

---
+++ src/lib/constants/userStatus.ts
@@ -0,0 +1,14 @@
+export const USER_INFORMATION = {
+  pending: "승인대기",
+  request: "승인요청",
+  approved: "승인됨",
+  rejected: "미승인",
+};
+
+export const USER_STATUS = {
+  name: "이름",
+  status: "상태",
+  phone: "전화번호",
+  request: "승인 요청",
+  id: "",
+};
