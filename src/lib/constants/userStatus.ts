d5237f554ab6d670ab16013068a1de6b3d24b21c
SeongYoon <namo4862@gmail.com>
2025-04-04 14:32:51 +0900

    feat: 한글명 변경에 사용할 상수 추가

--- src/lib/constants/userStatus.ts
+++ src/lib/constants/userStatus.ts
@@ -1,11 +1,11 @@
-export const USER_INFORMATION = {
+export const USER_STATUS = {
   pending: "승인대기",
   request: "승인요청",
   approved: "승인됨",
   rejected: "미승인",
 };
 
-export const USER_STATUS = {
+export const USER_INFORMATION = {
   name: "이름",
   status: "상태",
   phone: "전화번호",
