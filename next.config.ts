940d688363462ce451db0a567466534f32c6b6b3
SeongYoon <namo4862@gmail.com>
2025-04-08 17:16:35 +0900

    chore: NuxtImage remotePatterns 추가
    해당 호스트네임 이미지 불러오기 허용을 위함.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gym.tangoplus.co.kr",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
   webpack: (config, context) => {
     if (context.isServer) {
       if (Array.isArray(config.resolve.alias)) {
