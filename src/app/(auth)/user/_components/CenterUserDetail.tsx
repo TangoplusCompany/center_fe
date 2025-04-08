230c3b155becdd295d4175034512c89afe738cce
SeongYoon <namo4862@gmail.com>
2025-04-08 17:17:47 +0900

    fix: 유저 상세 페이지 수정
    상단 탭을통해 기번정보와 측정정보 구분

--- src/app/(auth)/user/_components/CenterUserDetail.tsx
+++ src/app/(auth)/user/_components/CenterUserDetail.tsx
@@ -1,23 +1,62 @@
 "use client";
 
-import MeasureInformation from "@/components/Measure/MeasureInformation";
-import SkeletonContainer from "@/components/Measure/SkeletonContainer";
 import { useUserDetail } from "@/hooks/user";
-import React from "react";
+import { useCallback, useState } from "react";
+import CenterUserInformation from "./CenterUserInformation";
+import CenterUserMeasure from "./CenterUserMeasure";
+
+const UserDetailTap = ({
+  nowTab,
+  update,
+}: {
+  nowTab: number;
+  update: (index: number) => void;
+}) => {
+  const handleClick = (value: number) => {
+    update(value);
+  };
+  return (
+    <ul className="flex items-center justify-start gap-5">
+      {["기본 정보", "측정 정보"].map((item, index) => {
+        return (
+          <li key={item + index}>
+            <button
+              type="button"
+              className={`${
+                nowTab === index
+                  ? "text-black dark:text-gray-200 border-b-2 border-black dark:border-gray-200 font-semibold"
+                  : "text-gray-400"
+              } text-lg`}
+              onClick={() => handleClick(index)}
+            >
+              {item}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 const CenterUserDetail = ({ sn }: { sn: number }) => {
   const { data, isLoading, error } = useUserDetail({ sn });
+  const [tab, setTab] = useState(0);
+  const handleTab = useCallback((index: number) => {
+    setTab(index);
+  }, []);
   if (isLoading) return <p>Loading...</p>;
   if (!data) return <p>No data</p>;
   if (error) {
     return <p>{error.message}</p>;
   }
   return (
-    <div className="w-full h-full flex flex-col">
-      <div className="w-full flex gap-10">
-        <SkeletonContainer data={data.measure_info} />
-        <MeasureInformation data={data.measure_info} />
-      </div>
+    <div className="w-full h-full flex flex-col gap-5 lg:gap-10">
+      <UserDetailTap nowTab={tab} update={handleTab} />
+      {tab ? (
+        <CenterUserMeasure measureData={data} />
+      ) : (
+        <CenterUserInformation data={data.measure_info} />
+      )}
     </div>
   );
 };
