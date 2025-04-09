5eae4cd4f4ab3793bc6f60166e19704c79cf8c85
SeongYoon <namo4862@gmail.com>
2025-04-09 15:14:55 +0900

    feat: useMeasureJson을 이용하여 pose landmark 구현

--- src/components/Measure/Static/Third.tsx
+++ src/components/Measure/Static/Third.tsx
@@ -1,7 +1,8 @@
 import { IUserDetailStatic } from "@/types/user";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ResultGraph from "../ResultGraph";
 import Image from "next/image";
+import { useMeasureJson } from "@/hooks/user";
 
 const MeasureStaticThird = ({
   className,
@@ -10,21 +11,234 @@ const MeasureStaticThird = ({
   className?: string;
   statics: IUserDetailStatic;
 }) => {
+  const defaultWidth = statics.measure_overlay_width as number;
+  const defaultHeight = statics.measure_overlay_height as number;
+  const [nowWidth, setNowWidth] = useState(defaultWidth);
+  const [nowHeight, setNowHeight] = useState(defaultHeight);
+  const [lineWidth, setLineWidth] = useState(1);
+  const [scaleWidth, setScaleWidth] = useState(1);
+  const [scaleHeight, setScaleHeight] = useState(1);
+  const imgRef = useRef<HTMLImageElement | null>(null);
+  const canvasWhiteRef = useRef<HTMLCanvasElement | null>(null);
+  const canvasRedRef = useRef<HTMLCanvasElement | null>(null);
+  const canvasGreenRef = useRef<HTMLCanvasElement | null>(null);
+  const { data, isLoading, isError } = useMeasureJson(
+    statics.measure_server_json_name,
+  );
+
+  useEffect(() => {
+    if (imgRef.current === null) return;
+    const updateCanvasScale = () => {
+      const imgWidth = imgRef.current!.width;
+      const imgHeight = imgRef.current!.height;
+
+      const widthScale = Number((imgWidth / defaultWidth).toFixed(4));
+      const heightScale = Number((imgHeight / defaultHeight).toFixed(4));
+
+      setLineWidth(imgWidth > 720 ? 2 : 1.5);
+      setNowWidth(imgWidth);
+      setNowHeight(imgHeight);
+      setScaleWidth(widthScale);
+      setScaleHeight(heightScale);
+    };
+
+    const drawCanvas = () => {
+      const canvasWhite = canvasWhiteRef.current!;
+      const canvasRed = canvasRedRef.current!;
+      const canvasGreen = canvasGreenRef.current!;
+
+      const contextWhite = canvasWhite.getContext("2d")!;
+      const contextRed = canvasRed.getContext("2d")!;
+      const contextGreen = canvasGreen.getContext("2d")!;
+
+      const clearAndDraw = (
+        context: CanvasRenderingContext2D,
+        color: string,
+        drawFn: () => void,
+      ) => {
+        context.clearRect(0, 0, canvasWhite.width, canvasWhite.height);
+        context.strokeStyle = color;
+        context.lineWidth = lineWidth;
+        drawFn();
+      };
+
+      clearAndDraw(contextWhite, "#FFF", () => {
+        contextWhite.beginPath();
+        contextWhite.moveTo(
+          data.pose_landmark[11].sx * scaleWidth,
+          data.pose_landmark[11].sy * scaleHeight,
+        );
+        contextWhite.lineTo(
+          data.pose_landmark[13].sx * scaleWidth,
+          data.pose_landmark[13].sy * scaleHeight,
+        );
+        contextWhite.lineTo(
+          data.pose_landmark[15].sx * scaleWidth,
+          data.pose_landmark[15].sy * scaleHeight,
+        );
+        contextWhite.stroke();
+
+        contextWhite.beginPath();
+        contextWhite.moveTo(
+          data.pose_landmark[11].sx * scaleWidth,
+          data.pose_landmark[11].sy * scaleHeight,
+        );
+        contextWhite.lineTo(
+          data.pose_landmark[23].sx * scaleWidth,
+          data.pose_landmark[23].sy * scaleHeight,
+        );
+        contextWhite.lineTo(
+          data.pose_landmark[25].sx * scaleWidth,
+          data.pose_landmark[25].sy * scaleHeight,
+        );
+        contextWhite.lineTo(
+          data.pose_landmark[27].sx * scaleWidth,
+          data.pose_landmark[27].sy * scaleHeight,
+        );
+        contextWhite.stroke();
+
+        contextWhite.beginPath();
+        contextWhite.moveTo(
+          data.pose_landmark[27].sx * scaleWidth,
+          data.pose_landmark[27].sy * scaleHeight,
+        );
+        contextWhite.lineTo(
+          data.pose_landmark[29].sx * scaleWidth,
+          data.pose_landmark[29].sy * scaleHeight,
+        );
+        contextWhite.moveTo(
+          data.pose_landmark[27].sx * scaleWidth,
+          data.pose_landmark[27].sy * scaleHeight,
+        );
+        contextWhite.lineTo(
+          data.pose_landmark[31].sx * scaleWidth,
+          data.pose_landmark[31].sy * scaleHeight,
+        );
+        contextWhite.stroke();
+      });
+
+      clearAndDraw(contextGreen, "#00FF00", () => {
+        contextGreen.beginPath();
+        contextGreen.moveTo(
+          data.pose_landmark[0].sx * scaleWidth,
+          data.pose_landmark[0].sy * scaleHeight,
+        );
+        contextGreen.lineTo(
+          data.pose_landmark[11].sx * scaleWidth,
+          data.pose_landmark[11].sy * scaleHeight,
+        );
+        contextGreen.stroke();
+
+        contextGreen.beginPath();
+        contextGreen.moveTo(
+          data.pose_landmark[15].sx * scaleWidth,
+          data.pose_landmark[15].sy * scaleHeight,
+        );
+        contextGreen.lineTo(
+          data.pose_landmark[27].sx * scaleWidth,
+          data.pose_landmark[15].sy * scaleHeight,
+        );
+        contextGreen.stroke();
+
+        contextGreen.beginPath();
+        contextGreen.moveTo(
+          data.pose_landmark[23].sx * scaleWidth,
+          data.pose_landmark[23].sy * scaleHeight,
+        );
+        contextGreen.lineTo(
+          data.pose_landmark[27].sx * scaleWidth,
+          data.pose_landmark[23].sy * scaleHeight,
+        );
+        contextGreen.stroke();
+
+        contextGreen.beginPath();
+        contextGreen.moveTo(
+          data.pose_landmark[25].sx * scaleWidth,
+          data.pose_landmark[25].sy * scaleHeight,
+        );
+        contextGreen.lineTo(
+          data.pose_landmark[27].sx * scaleWidth,
+          data.pose_landmark[25].sy * scaleHeight,
+        );
+        contextGreen.stroke();
+      });
+
+      clearAndDraw(contextRed, "#FF0000", () => {
+        contextRed.beginPath();
+        contextRed.moveTo(
+          data.pose_landmark[11].sx * scaleWidth,
+          data.pose_landmark[0].sy * scaleHeight - 100,
+        );
+        contextRed.lineTo(
+          data.pose_landmark[27].sx * scaleWidth,
+          data.pose_landmark[27].sy * scaleHeight + 100,
+        );
+        contextRed.stroke();
+
+        contextRed.beginPath();
+        contextRed.moveTo(
+          data.pose_landmark[11].sx * scaleWidth - 100,
+          data.pose_landmark[11].sy * scaleHeight,
+        );
+        contextRed.lineTo(
+          data.pose_landmark[11].sx * scaleWidth + 100,
+          data.pose_landmark[11].sy * scaleHeight,
+        );
+        contextRed.stroke();
+
+        contextRed.beginPath();
+        contextRed.moveTo(
+          data.pose_landmark[0].sx * scaleWidth - 100,
+          data.pose_landmark[0].sy * scaleHeight,
+        );
+        contextRed.lineTo(
+          data.pose_landmark[0].sx * scaleWidth + 100,
+          data.pose_landmark[0].sy * scaleHeight,
+        );
+        contextRed.stroke();
+      });
+    };
+    updateCanvasScale();
+    drawCanvas();
+  }, [imgRef.current, data]);
+
+  if (!data) return <div>데이터가 없습니다.</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>에러가 발생했습니다.</div>;
   return (
-    <div className={`${className} flex flex-col gap-4`}>
-      <div className="relative w-full h-[400px] overflow-hidden lg:h-[600px]">
+    <div className={`${className} flex flex-col gap-4 lg:gap-10`}>
+      <div className="relative w-full overflow-hidden">
         <Image
+          ref={imgRef}
           src={
             `https://gym.tangoplus.co.kr/data/Results/` +
             statics.measure_server_file_name
           }
           alt="측정 사진"
           width={1500}
-          height={422}
-          className="lg:w-full lg:relative absolute top-0 left-0 w-[750px] h-[400px] object-cover lg:h-full"
+          height={844}
+          className="lg:w-full lg:relative absolute top-0 left-0 w-[750px] h-[400px] object-cover lg:h-full -scale-x-[1]"
+        />
+        <canvas
+          ref={canvasWhiteRef}
+          width={nowWidth}
+          height={nowHeight}
+          className="absolute bottom-0 left-0 right-0 top-0 z-9"
+        />
+        <canvas
+          ref={canvasGreenRef}
+          width={nowWidth}
+          height={nowHeight}
+          className="absolute bottom-0 left-0 right-0 top-0 z-9"
+        />
+        <canvas
+          ref={canvasRedRef}
+          width={nowWidth}
+          height={nowHeight}
+          className="absolute bottom-0 left-0 right-0 top-0 z-9"
         />
       </div>
-      <div className="grid flex-1 grid-cols-12 gap-2 md:gap-5 w-full lg:gap-10">
+      <div className="grid flex-1 grid-cols-12 gap-2 md:gap-5 w-full lg:gap-5">
         <div className="col-span-12 flex lg:flex-row flex-col items-start gap-5 text-black dark:text-white">
           <ResultGraph
             defaultAvg={32.55}
