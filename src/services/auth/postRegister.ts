de9bbcf9d4d835f550bfcdf897fa893ddb99d3c0
SeongYoon <namo4862@gmail.com>
2025-04-15 18:05:54 +0900

    feat: Login, Register를 위한 서비스 분리
    이후 모든 서비스 분리예정

---
+++ src/services/auth/postRegister.ts
@@ -0,0 +1,21 @@
+import { customAuthAxios } from "@/lib/axios";
+
+export const postRegister = async ({
+  email,
+  mobile,
+  password,
+  name,
+}: {
+  email: string;
+  mobile: string;
+  password: string;
+  name: string;
+}) => {
+  const response = await customAuthAxios.post("/auth/register", {
+    email,
+    password,
+    mobile,
+    name,
+  });
+  return response.data;
+};
