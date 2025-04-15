dc46292cb99dda483992b2f1e1b9be02906dea04
SeongYoon <namo4862@gmail.com>
2025-04-15 18:04:00 +0900

    fix: prettier로 인한 수정

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,14 +1,7 @@
 "use client";
 
 import React from "react";
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export default function Loading() {
-  return (
-    <DotLottieReact
-      src="https://lottie.host/c00e741b-9f42-4bd7-b746-fb1254979fa5/z3I8mzfZrq.lottie"
-      loop
-      autoplay
-    />
-  );
+  return <div></div>;
 }
