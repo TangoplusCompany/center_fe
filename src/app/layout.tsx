451da122601d4c0ed67cfc282600169f303222eb
SeongYoon <namo4862@gmail.com>
2025-04-17 09:31:58 +0900

    🔥 remove: MSW

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-
-import MockerProvider from "@/mocks/MockerProvider";
 import QueryProvider from "@/providers/QueryProvider";
 
 const geistSans = Geist({
@@ -28,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <ThemeProvider
           attribute={"class"}
           defaultTheme="system"
