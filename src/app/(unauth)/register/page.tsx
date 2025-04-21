2b873fd9d662b58ae0dbf6cdce60e3131a4a8e05
SeongYoon <namo4862@gmail.com>
2025-04-21 10:41:51 +0900

    🐛 fix: 센터아이디 검증에 따른 수정

--- src/app/(unauth)/register/page.tsx
+++ src/app/(unauth)/register/page.tsx
@@ -4,7 +4,7 @@ import { RegisterContainer } from "../_components/RegisterForm";
 const RegisterPage = () => {
   return (
     <div className="flex flex-1 items-center justify-center">
-      <div className="w-full max-w-xs">
+      <div className="w-full max-w-[480px]">
         <RegisterContainer />
       </div>
     </div>
--- src/services/auth/postRegister.ts
+++ src/services/auth/postRegister.ts
@@ -1,17 +1,20 @@
 import { customUnAuthAxios } from "@/lib/axios";
 
 export const postRegister = async ({
+  center_id,
   email,
   mobile,
   password,
   name,
 }: {
+  center_id: string;
   email: string;
   mobile: string;
   password: string;
   name: string;
 }) => {
   const response = await customUnAuthAxios.post("/auth/register", {
+    center_id,
     email,
     password,
     mobile,
