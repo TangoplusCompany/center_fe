a81049bcced0180e93337f9ca8ab0e21d80b45e1
SeongYoon <namo4862@gmail.com>
2025-04-21 10:41:07 +0900

    feat: CenterId 체크하는 API/Service

---
+++ src/hooks/center/useCenterCheck.ts
@@ -0,0 +1,37 @@
+import { getCenterCheck } from "@/services/center/getCenterCheck";
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+
+interface ICenterCheck {
+  status: number;
+  success: boolean;
+  message: string[];
+}
+
+type Props = {
+  onCenterCheck: (centerId: string) => void;
+  centerCode: string;
+};
+
+export const useCenterCheck = ({ onCenterCheck, centerCode }: Props) => {
+  return useMutation({
+    mutationFn: getCenterCheck,
+    onSuccess: (data: ICenterCheck) => {
+      onCenterCheck(centerCode);
+      // Handle successful login, e.g., redirect to dashboard
+    },
+    onError: (
+      data: AxiosError<{
+        data: ICenterCheck;
+        message: string[];
+        status: number;
+        success: boolean;
+      }>,
+    ) => {
+      if (data.status === 404) {
+        alert("해당 센터가 존재하지 않습니다. 다시한번 확인 바랍니다.");
+      }
+      return;
+    },
+  });
+};
