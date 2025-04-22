28f2e26f9dee2e99c026c0cead88a1bcd2d461bf
SeongYoon <namo4862@gmail.com>
2025-04-22 18:32:35 +0900

    ✨ feat : 측정 상세조회 컴포넌트들 구현

--- src/components/Measure/MeasureInformation.tsx
+++ src/components/Measure/MeasureInformation.tsx
@@ -1,30 +1,21 @@
-import { IFilterMeasureInfo } from "@/types/user";
+import { IUserDetailMeasureInfo } from "@/types/user";
+import { parseString } from "@/utils/parseString";
 import React from "react";
 
-const MeasureInformation = ({ data }: { data: IFilterMeasureInfo }) => {
+const MeasureInformation = ({ data }: { data: IUserDetailMeasureInfo }) => {
   return (
     <div className="flex-1">
       <h1 className="text-4xl font-semibold text-[#333]">
-        {data.information.user_name}님의 측정 내역
+        {data.user_name}님의 측정 내역
       </h1>
-      {Object.entries(data["information"]).map(([key, value]) => {
-        return (
-          <div key={key} className="flex gap-2">
-            <p>{key}</p>
-            <p>:</p>
-            <p>{value}</p>
-          </div>
-        );
-      })}
-      {Object.entries(data["risk"]).map(([key, value]) => {
-        return (
-          <div key={key} className="flex gap-2">
-            <p>{key}</p>
-            <p>:</p>
-            <p>{value}</p>
-          </div>
-        );
-      })}
+      <div className="flex flex-col gap-2">
+        <p>측정점수 : {data.t_score}점</p>
+        <div>
+          {parseString(data.risk_result_ment).map((el, key) => {
+            return el === "" ? <br key={key} /> : <p key={key}>{el}</p>;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
