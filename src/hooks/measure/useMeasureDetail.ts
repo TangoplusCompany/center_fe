d0d92d1b50f2c19abc62ccff132a9d657b29f34a
SeongYoon <namo4862@gmail.com>
2025-04-22 18:34:13 +0900

    ✨ feat: useMeasureDetail API Hooks

---
+++ src/hooks/measure/useMeasureDetail.ts
@@ -0,0 +1,16 @@
+import { customAxios } from "@/lib/axios";
+import { useQuery } from "@tanstack/react-query";
+
+export const useMeasureDetail = <T>(sn: number, user_uuid: string) => {
+  return useQuery<T>({
+    queryKey: ["measureDetail", sn, user_uuid],
+    queryFn: async () => {
+      const response = await customAxios.get(`/measurement/${sn}`, {
+        params: {
+          user_uuid,
+        },
+      });
+      return response.data.data;
+    },
+  });
+};
