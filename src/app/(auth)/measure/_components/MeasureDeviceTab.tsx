c45470bb2acf837cc87e80c7988f4ed6d94169b6
SeongYoon <namo4862@gmail.com>
2025-04-22 18:27:19 +0900

    ✨ feat: 측정 리스트 조회시 필요한 device tab 구현

---
+++ src/app/(auth)/measure/_components/MeasureDeviceTab.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useGetDeviceStatus } from "@/hooks/device";
+import { useQueryParams } from "@/hooks/utils/useQueryParams";
+import { IDeviceStatus } from "@/types/device";
+import React from "react";
+
+const MeasureDeviceTab = () => {
+  const { query, setQueryParam } = useQueryParams();
+  const deviceSn = query.device_sn || 0;
+  const {
+    data: measureDeviceResponse,
+    isLoading,
+    isError,
+  } = useGetDeviceStatus<IDeviceStatus>();
+
+  const handleDeviceClick = (deviceSn: number) => {
+    setQueryParam("device_sn", deviceSn);
+  };
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error...</div>;
+  if (!measureDeviceResponse) return <div>No data</div>;
+  return (
+    <div className="flex items-center justify-start gap-3">
+      {measureDeviceResponse.data.map((device, index) => {
+        return (
+          <Button
+            key={device.device_name + index}
+            className="cursor-pointer"
+            variant={deviceSn === device.sn ? "default" : "outline"}
+            onClick={() => handleDeviceClick(device.sn)}
+          >
+            {device.device_name}
+          </Button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default MeasureDeviceTab;
