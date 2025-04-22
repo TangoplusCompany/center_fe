f1b02bb537120b4610ba70bce7159214fb9a06ed
SeongYoon <namo4862@gmail.com>
2025-04-22 09:11:31 +0900

    🔨 fix: refetch 선언위치 수정

--- src/hooks/device/useDeviceAdd.ts
+++ src/hooks/device/useDeviceAdd.ts
@@ -2,12 +2,13 @@ import { postDeviceAdd } from "@/services/device/postDeviceAdd";
 import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 
-export const useDeviceAdd = () => {
+export const useDeviceAdd = (refetch: () => void) => {
   return useMutation({
     mutationFn: postDeviceAdd,
     onSuccess: () => {
       // Handle successful login, e.g., redirect to dashboard
       console.log("Device added successfully");
+      refetch();
     },
     onError: (
       data: AxiosError<{
