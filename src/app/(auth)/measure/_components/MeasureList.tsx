f577b0cff3810d4d8baa8ad08077f48dbe0b8a15
SeongYoon <namo4862@gmail.com>
2025-04-22 18:26:58 +0900

    ✨ feat: 측정 조회 리스트 컴포넌트 구현

---
+++ src/app/(auth)/measure/_components/MeasureList.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { IMeasureList } from "@/types/measure";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
+import { FileText } from "lucide-react";
+import { nameFiltering, phoneFiltering } from "@/utils/regexFiltering";
+import { formatDate } from "@/utils/formatDate";
+
+export const MeasureList = ({
+  measurements,
+}: {
+  measurements: IMeasureList[];
+}) => {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="text-center w-[100px]">이름</TableHead>
+          <TableHead className="text-center">전화번호</TableHead>
+          <TableHead className="text-center">측정일</TableHead>
+          <TableHead className="text-center">측정점수</TableHead>
+          <TableHead className="text-right"></TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {measurements.map((measurement) => (
+          <TableRow key={measurement.sn}>
+            <TableCell className="text-center font-medium">
+              {measurement.user_name
+                ? nameFiltering(measurement.user_name)
+                : ""}
+            </TableCell>
+            <TableCell className="text-center ">
+              {phoneFiltering(measurement.mobile)}
+            </TableCell>
+            <TableCell className="text-center">
+              {formatDate(measurement.measure_date)}
+            </TableCell>
+            <TableCell className="text-center">
+              {measurement.t_score}점
+            </TableCell>
+            <TableCell className="text-right">
+              <Link
+                href={`/measure/${measurement.sn}?user_uuid=${measurement.user_uuid}`}
+                className="flex items-center gap-2 justify-end cursor-pointer"
+              >
+                <FileText className="w-4 h-4" />
+                <span>상세보기</span>
+              </Link>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
