c06fdfe22c9de569ee1d7993b1e8c5f947980305
SeongYoon <namo4862@gmail.com>
2025-04-23 17:54:04 +0900

    ✨ feat: 유저 추가 페이지 구현

---
+++ src/hooks/user/useAddUser.ts
@@ -0,0 +1,27 @@
+import { postAddUser } from "@/services/user/postAddUser";
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+
+export const useAddUser = () => {
+  const router = useRouter();
+  return useMutation({
+    mutationFn: postAddUser,
+    onSuccess: (data) => {
+      console.log(data);
+      alert("사용자 추가에 성공했습니다.");
+      router.push("/user");
+    },
+    onError: (
+      data: AxiosError<{
+        data: unknown;
+        message: string[];
+        status: number;
+        success: boolean;
+      }>,
+    ) => {
+      alert(`Error: ${data.message}`);
+      return;
+    },
+  });
+};
