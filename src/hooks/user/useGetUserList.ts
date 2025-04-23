3ba79c6143fd960034cb7a147da0a9bf762ee5e7
SeongYoon <namo4862@gmail.com>
2025-04-23 17:54:51 +0900

    🔨 fix: response data에 맞게 api hooks 수정

--- src/hooks/user/useGetUserList.ts
+++ src/hooks/user/useGetUserList.ts
@@ -1,11 +1,22 @@
 import { customAxios } from "@/lib/axios";
 import { useQuery } from "@tanstack/react-query";
 
-export const useGetUserList = <T>() =>
+export const useGetUserList = <T>({
+  page,
+  limit,
+}: {
+  page: number;
+  limit: number;
+}) =>
   useQuery<T>({
-    queryKey: ["UserMain"],
+    queryKey: ["CenterUserList"],
     queryFn: async () => {
-      const response = await customAxios.get("/members");
+      const response = await customAxios.get("/members", {
+        params: {
+          page,
+          limit,
+        },
+      });
       return response.data.data;
     },
   });
