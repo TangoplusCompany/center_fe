3523dcd4789b7a8133417276f682e3c9402e40f6
SeongYoon <namo4862@gmail.com>
2025-04-24 11:28:08 +0900

    ✏️ chore: 주석수정

--- src/services/auth/postRefreshAccessToken.ts
+++ src/services/auth/postRefreshAccessToken.ts
@@ -2,11 +2,11 @@ import { customUnAuthAxios } from "@/lib/axios";
 
 /**
  * 쿠키에 값을 저장해두고, 해당 쿠키를 이용해서 요청 받기
- * @returns Promise{ accessToken: string, refreshToken: string }
+ * @returns Promise{ success: boolean, status: number, message: string[], data: {access_jwt: string} }
  */
 export const refreshAccessToken = async () => {
   const res = await customUnAuthAxios.post("/auth/refresh", null, {
     withCredentials: true,
   });
-  return res.data; // { accessToken: "..." }
+  return res.data;
 };
