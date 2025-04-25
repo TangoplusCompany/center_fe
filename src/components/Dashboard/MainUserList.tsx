04437201545b187215ba51e5ab2f9e8118b502e5
SeongYoon <namo4862@gmail.com>
2025-04-25 15:53:46 +0900

    🔨 fix: 최근 측정 조회와 유저리스트 수정

--- src/components/Dashboard/MainUserList.tsx
+++ src/components/Dashboard/MainUserList.tsx
@@ -13,56 +13,95 @@ import { FileText } from "lucide-react";
 import { phoneHyphen } from "@/utils/regexFiltering";
 import { useEffect, useState } from "react";
 import { IUserData } from "@/types/user";
+import { IMeasureList } from "@/types/measure";
+import { formatDate } from "@/utils/formatDate";
 
 export const MainUserList = ({
   users,
   path,
 }: {
-  users: IUserData[];
+  users: IUserData[] | IMeasureList[];
   path: string;
 }) => {
-  const [list, setList] = useState<IUserData[]>(users);
+  const [list, setList] = useState<IUserData[] | IMeasureList[]>(users);
 
   useEffect(() => {
     setList(users);
   }, [users]);
   return (
     <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="text-center w-[100px]">이름</TableHead>
-          <TableHead className="text-center">전화번호</TableHead>
-          <TableHead className="text-center">이메일</TableHead>
-          <TableHead className="text-right"></TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {list.map((user) => (
-          <TableRow key={user.user_uuid}>
-            <TableCell className="text-center font-medium">
-              {user.user_name}
-            </TableCell>
-            <TableCell className="text-center ">
-              {phoneHyphen(user.mobile)}
-            </TableCell>
+      {path === "user" && (
+        <>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="text-center w-[100px]">이름</TableHead>
+              <TableHead className="text-center">전화번호</TableHead>
+              <TableHead className="text-center">이메일</TableHead>
+              <TableHead className="text-right"></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {(list as IUserData[]).map((user) => (
+              <TableRow key={user.user_uuid}>
+                <TableCell className="text-center font-medium">
+                  {user.user_name}
+                </TableCell>
+                <TableCell className="text-center ">
+                  {phoneHyphen(user.mobile)}
+                </TableCell>
 
-            <TableCell className="text-center">{user.email}</TableCell>
-            <TableCell className="flex items-center justify-end gap-2">
-              <Link
-                href={`${
-                  path === "user"
-                    ? `/user/${user.user_uuid}`
-                    : `/measure/${user.user_sn}?user_uuid=${user.user_uuid}`
-                }`}
-                className="flex items-center gap-2 justify-end cursor-pointer"
-              >
-                <FileText className="w-4 h-4" />
-                <span>상세보기</span>
-              </Link>
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
+                <TableCell className="text-center">{user.email}</TableCell>
+                <TableCell className="flex items-center justify-end gap-2">
+                  <Link
+                    href={`/user/${user.user_uuid}`}
+                    className="flex items-center gap-2 justify-end cursor-pointer"
+                  >
+                    <FileText className="w-4 h-4" />
+                    <span>상세보기</span>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </>
+      )}
+      {path === "measure" && (
+        <>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="text-center w-[100px]">이름</TableHead>
+              <TableHead className="text-center">점수</TableHead>
+              <TableHead className="text-center">측정일</TableHead>
+              <TableHead className="text-right"></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {(list as IMeasureList[]).map((measure) => (
+              <TableRow key={measure.user_uuid}>
+                <TableCell className="text-center font-medium">
+                  {measure.user_name}
+                </TableCell>
+                <TableCell className="text-center ">
+                  {measure.t_score}점
+                </TableCell>
+
+                <TableCell className="text-center">
+                  {formatDate(measure.measure_date)}
+                </TableCell>
+                <TableCell className="flex items-center justify-end gap-2">
+                  <Link
+                    href={`/measure/${measure.sn}?user_uuid=${measure.user_uuid}`}
+                    className="flex items-center gap-2 justify-end cursor-pointer"
+                  >
+                    <FileText className="w-4 h-4" />
+                    <span>상세보기</span>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </>
+      )}
     </Table>
   );
 };
