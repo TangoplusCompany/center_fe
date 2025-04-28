2f1ce26f6f36def5be60efa8d38ce816643b8470
SeongYoon <namo4862@gmail.com>
2025-04-28 16:00:57 +0900

    🔨 fix: 매니저 리스트 수정

--- src/components/Center/CenterManagerList.tsx
+++ src/components/Center/CenterManagerList.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useGetManagerList } from "@/hooks/auth/useGetManagerList";
 import React from "react";
 import {
   Table,
@@ -13,13 +12,15 @@ import {
 import { phoneHyphen } from "@/utils/regexFiltering";
 import { FileText } from "lucide-react";
 import Link from "next/link";
+import { ICenterManagerData } from "@/types/manager";
+import { ADMIN_ROLE } from "@/utils/constants";
+import ManagerRemoveDialog from "../Manager/ManagerRemoveDialog";
 
-const CenterManagerList = () => {
-  const { data: managerList, isLoading, isError } = useGetManagerList();
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error loading data</div>;
-  if (!managerList) return <div>No data available</div>;
-
+const CenterManagerList = ({
+  managerList,
+}: {
+  managerList: ICenterManagerData[];
+}) => {
   return (
     <Table>
       <TableHeader>
@@ -27,6 +28,7 @@ const CenterManagerList = () => {
           <TableHead className="text-center w-[100px]">이름</TableHead>
           <TableHead className="text-center">전화번호</TableHead>
           <TableHead className="text-center">이메일</TableHead>
+          <TableHead className="text-center">등급</TableHead>
           <TableHead className="text-right"></TableHead>
         </TableRow>
       </TableHeader>
@@ -41,14 +43,18 @@ const CenterManagerList = () => {
             </TableCell>
 
             <TableCell className="text-center">{manager.admin_email}</TableCell>
+            <TableCell className="text-center">
+              {ADMIN_ROLE[manager.admin_role as keyof typeof ADMIN_ROLE]}
+            </TableCell>
             <TableCell className="flex items-center justify-end gap-2">
               <Link
-                href={`/setting/admin/${manager.sn}`}
+                href={`/manager/${manager.sn}`}
                 className="flex items-center gap-2 justify-end cursor-pointer"
               >
                 <FileText className="w-4 h-4" />
                 <span>상세보기</span>
               </Link>
+              <ManagerRemoveDialog manager={manager} />
             </TableCell>
           </TableRow>
         ))}
