e69b7cf863172ae51533d5633c6189aecf63f40d
SeongYoon <namo4862@gmail.com>
2025-04-30 11:57:12 +0900

    🔨 fix: 더미데이터 걷어내고 API데이터로 차트 작성

--- src/components/Dashboard/DeviceChartContainer.tsx
+++ src/components/Dashboard/DeviceChartContainer.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { useDeviceChartConfig } from "@/hooks/device/useDeviceChartConfig";
-import { IDeviceChartResponse, IDeviceStatusCardProps } from "@/types/device";
+import { IDeviceStatusCardProps } from "@/types/device";
 import React from "react";
 import DeviceChart from "./DeviceChart";
-
 import { useDevicePeriodData } from "@/hooks/device/useDevicePeriodData";
-import { PERIOD_OPTIONS } from "@/utils/constants";
 
 const DeviceChartContainer = ({
   deviceList,
@@ -14,17 +12,15 @@ const DeviceChartContainer = ({
   deviceList: IDeviceStatusCardProps[];
 }) => {
   const { chartConfig } = useDeviceChartConfig(deviceList);
-  const [period, setPeriod] = React.useState(PERIOD_OPTIONS);
-  const { data: chartResponse, isLoading } =
-    useDevicePeriodData<IDeviceChartResponse>(period[0].value);
+  const { data: chartData, isLoading } = useDevicePeriodData("1m");
   if (isLoading) return <div>Loading...</div>;
-  if (!chartResponse) return <div>No data</div>;
+  if (!chartData) return <div>No data</div>;
 
   return (
     <div className="w-full flex flex-col gap-4">
       <h2 className="text-2xl">키오스크 사용자 추이</h2>
       <div className="w-full">
-        <DeviceChart chartConfig={chartConfig} chartData={chartResponse.data} />
+        <DeviceChart chartConfig={chartConfig} chartData={chartData} />
       </div>
     </div>
   );
