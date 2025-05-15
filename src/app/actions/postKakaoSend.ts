636666f08382a4d2c7803b6ca1e85d0ddeca0cab
SeongYoon <namo4862@gmail.com>
2025-05-15 18:07:29 +0900

    chore: 카카오 안보내지게 임시 수정

--- src/app/actions/postKakaoSend.ts
+++ src/app/actions/postKakaoSend.ts
@@ -4,6 +4,6 @@ import axios from "axios";
 
 export const postKakaoSend = async (data: string) => {
   const kakaoURL = process.env.NEXT_PUBLIC_KAKAO_URL as string;
-  const response = await axios.post(`${kakaoURL}?link=${encodeURIComponent(data)}`);
-  return response.data;
+  // const response = await axios.post(`${kakaoURL}?link=${encodeURIComponent(data)}`);
+  return kakaoURL;
 };
