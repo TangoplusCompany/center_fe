dce632d2bf3020380383005a5b4dc79d5e3a752c
SeongYoon <namo4862@gmail.com>
2025-05-15 18:07:54 +0900

    docs: 설명 주석 추가

--- src/hooks/user/useGetUserMeasureList.ts
+++ src/hooks/user/useGetUserMeasureList.ts
@@ -1,6 +1,13 @@
 import { customAxios } from "@/lib/axios";
 import { useQuery } from "@tanstack/react-query";
 
+/**
+ * 유저 측정 목록 조회
+ * @param page 페이지 번호
+ * @param limit 페이지 당 측정 목록 개수
+ * @param user_uuid 유저 고유 아이디
+ * @returns 유저 측정 목록 데이터
+ */
 export const useGetUserMeasureList = <T>({
   page,
   limit,
