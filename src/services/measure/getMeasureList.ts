5c06108adfec12c699c01d5623f5bb2a388e78c8
SeongYoon <namo4862@gmail.com>
2025-05-16 16:31:52 +0900

    fix: search 적용을 위한 hooks, service 개선

--- src/services/measure/getMeasureList.ts
+++ src/services/measure/getMeasureList.ts
@@ -1,21 +1,29 @@
 import { customAxios } from "@/lib/axios";
 
-// 센터 등록할 기기 찾기
+// 센터 측정리스트
 export const getMeasureList = async ({
   page,
   limit,
   deviceSn,
+  search,
 }: {
-  page: number;
-  limit: number;
-  deviceSn: string;
+  page?: number;
+  limit?: number;
+  deviceSn?: string;
+  search?: string;
 }) => {
+  const params: Record<string, string | number> = {
+    page: page || 1,
+    limit: limit || 20,
+  };
+  if (search?.trim() && search !== "") {
+    params.search = search;
+  }
+  if (deviceSn?.trim() && deviceSn !== "") {
+    params.device_sn = deviceSn;
+  }
   const { data } = await customAxios.get(`/measurement`, {
-    params: {
-      page,
-      limit,
-      device_sn: deviceSn,
-    },
+    params,
   });
   return data;
 };
