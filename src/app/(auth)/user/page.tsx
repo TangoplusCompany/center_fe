55677d4069707cc415c431c9fb428d32d21742e9
SeongYoon <namo4862@gmail.com>
2025-05-19 21:26:35 +0900

    fix

--- src/app/(auth)/user/page.tsx
+++ src/app/(auth)/user/page.tsx
@@ -5,7 +5,7 @@ import UserPage from "./_components/UserPage";
 
 const UserHome = () => {
   return (
-    <div className="grid grid-cols-12 gap-5">
+    <div className="grid grid-cols-12 gap-5 relative">
       <h1 className="text-2xl col-span-12">센터 사용자 관리</h1>
       <UserPage />
     </div>
--- src/components/User/CenterUserInformation.tsx
+++ src/components/User/CenterUserInformation.tsx
@@ -4,14 +4,19 @@ import { useGetUserDetail } from "@/hooks/user";
 import { useGetQuery } from "@/hooks/utils/useGetQuery";
 import React from "react";
 import UserDetailForm from "./UserDetailForm";
+import DataError from "../Util/DataError";
 
 const CenterUserInformation = () => {
   const { query } = useGetQuery();
   const { key } = query as { key: string };
-  const { data: userDetailData, isLoading: userDetailDataLoading } =
-    useGetUserDetail({ userSn: key });
+  const {
+    data: userDetailData,
+    isLoading: userDetailDataLoading,
+    isError: userDetailError,
+  } = useGetUserDetail({ userSn: key });
 
   if (userDetailDataLoading) return <p>사용자 정보 불러오는중...</p>;
+  if (userDetailError) return <DataError></DataError>;
   if (!userDetailData) return <></>;
 
   return (
