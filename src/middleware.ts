a71422714a87c8e421ee3db301449582f7b8c4f8
SeongYoon <namo4862@gmail.com>
2025-05-19 21:24:13 +0900

    fix: middleware result-sheet page 예외 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,6 +11,6 @@ export function middleware(request: NextRequest) {
 }
 export const config = {
   matcher: [
-    "/((?!login|register|_next|favicon.ico).*)", // ← ✅ 이렇게 써야 작동함
+    "/((?!login|result-sheet|register|_next|favicon.ico).*)",
   ],
 };
