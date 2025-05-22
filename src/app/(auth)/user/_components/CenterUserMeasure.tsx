7c51103bb778dd453ea653f5f1a109683369b1d4
SeongYoon <namo4862@gmail.com>
2025-05-22 16:40:06 +0900

    fix: 사용자 측정조회 수정

--- src/app/(auth)/user/_components/CenterUserMeasure.tsx
+++ src/app/(auth)/user/_components/CenterUserMeasure.tsx
@@ -14,6 +14,9 @@ import MeasureStaticThird from "@/components/Measure/Static/Third";
 import MeasureStaticFourth from "@/components/Measure/Static/Fourth";
 import MeasureStaticFifth from "@/components/Measure/Static/Fifth";
 import MeasureStaticSixth from "@/components/Measure/Static/Sixth";
+import BackMeasurement from "@/components/Measure/Static/BackMeasurement";
+import FrontMeasurement from "@/components/Measure/Static/FrontMeasurement";
+import SideMeasurement from "@/components/Measure/Static/SideMeasurement";
 
 type MeasureListType = {
   title: string;
@@ -27,35 +30,70 @@ const CenterUserMeasure = ({
   measureData: IUserMeasurement;
 }) => {
   const measureList: MeasureListType[] = [
+    // {
+    //   title: "정면 측정",
+    //   value: "front",
+    //   component: () => <MeasureStaticFirst statics={measureData.static_1} />,
+    // },
+    // {
+    //   title: "팔꿉 측정",
+    //   value: "elbow",
+    //   component: () => <MeasureStaticSecond statics={measureData.static_2} />,
+    // },
+    // {
+    //   title: "좌측 측정",
+    //   value: "left",
+    //   component: () => <MeasureStaticThird statics={measureData.static_3} />,
+    // },
+    // {
+    //   title: "우측 측정",
+    //   value: "right",
+    //   component: () => <MeasureStaticFourth statics={measureData.static_4} />,
+    // },
+    // {
+    //   title: "후면 측정",
+    //   value: "back",
+    //   component: () => <MeasureStaticFifth statics={measureData.static_5} />,
+    // },
+    // {
+    //   title: "후면 앉은 측정",
+    //   value: "back_sit",
+    //   component: () => <MeasureStaticSixth statics={measureData.static_6} />,
+    // },
+    // {
+    //   title: "스쿼트 측정",
+    //   value: "dynamic",
+    //   component: () => <MeasureDetailDynamic dynamic={measureData.dynamic} />,
+    // },
     {
       title: "정면 측정",
-      value: "front",
-      component: () => <MeasureStaticFirst statics={measureData.static_1} />,
+      value: "frontTotal",
+      component: () => (
+        <FrontMeasurement
+          statics_1={measureData.static_1}
+          statics_2={measureData.static_2}
+        />
+      ),
     },
     {
-      title: "팔꿉 측정",
-      value: "elbow",
-      component: () => <MeasureStaticSecond statics={measureData.static_2} />,
-    },
-    {
-      title: "좌측 측정",
-      value: "left",
-      component: () => <MeasureStaticThird statics={measureData.static_3} />,
-    },
-    {
-      title: "우측 측정",
-      value: "right",
-      component: () => <MeasureStaticFourth statics={measureData.static_4} />,
+      title: "측면 측정",
+      value: "SideTotal",
+      component: () => (
+        <SideMeasurement
+          statics_3={measureData.static_3}
+          statics_4={measureData.static_4}
+        />
+      ),
     },
     {
       title: "후면 측정",
-      value: "back",
-      component: () => <MeasureStaticFifth statics={measureData.static_5} />,
-    },
-    {
-      title: "후면 앉은 측정",
-      value: "back_sit",
-      component: () => <MeasureStaticSixth statics={measureData.static_6} />,
+      value: "BackTotal",
+      component: () => (
+        <BackMeasurement
+          statics_5={measureData.static_5}
+          statics_6={measureData.static_6}
+        />
+      ),
     },
     {
       title: "스쿼트 측정",
@@ -64,7 +102,7 @@ const CenterUserMeasure = ({
     },
   ];
   return (
-    <Tabs defaultValue="front" className="w-full">
+    <Tabs defaultValue="frontTotal" className="w-full">
       <TabsList>
         {measureList.map((measure: Omit<MeasureListType, "component">) => (
           <TabsTrigger key={measure.value} value={measure.value}>
