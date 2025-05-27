bd75daeb8ad87c8d2449b0facf272288be42e691
SeongYoon <namo4862@gmail.com>
2025-05-27 10:01:12 +0900

    feat: webpack alias

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
+import { resolve } from "path";
 const nextConfig: NextConfig = {
+  webpack(config: any) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": resolve(__dirname, "src"),
+    };
+    return config;
+  },
   images: {
     remotePatterns: [
       {
