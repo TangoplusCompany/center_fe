576031b71036ebb197019a1e7d031cb67117eadc
SeongYoon <namo4862@gmail.com>
2025-06-11 15:56:52 +0900

    feat: 로그인 연장기능 추가

---
+++ src/components/Util/UpdateSessionButton.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useLoginTimeout } from "@/hooks/utils/useLoginTimeout";
+import { Button } from "../ui/button";
+import { formatTime } from "@/utils/formatDate";
+
+const UpdateSessionButton = () => {
+  const { remainingSeconds, resetTimer } = useLoginTimeout();
+
+  const handleUpdateSession = () => {
+    resetTimer();
+    alert("로그인이 연장되었습니다.");
+  };
+  console.log(remainingSeconds);
+  return (
+    <div className="flex items-center gap-2 md:gap-4">
+      <p className="">
+        남은 시간: <strong>{formatTime(remainingSeconds)}</strong>
+      </p>
+      <Button onClick={handleUpdateSession} variant="outline">로그인 연장</Button>
+    </div>
+  );
+};
+
+export default UpdateSessionButton;
