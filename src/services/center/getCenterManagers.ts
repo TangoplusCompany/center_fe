61f23a9fac1f8af4a0c062b4baa46cac315824de
SeongYoon <namo4862@gmail.com>
2025-06-24 15:54:18 +0900

    fix: 매니저 검색 기능 추가

--- src/services/center/getCenterManagers.ts
+++ src/services/center/getCenterManagers.ts
@@ -1,17 +1,23 @@
 import { customAxios } from "@/lib/axios";
 import { IResponseDefault } from "@/types/default";
-import { ICenterManagerData } from "@/types/manager";
+import { ICenterManagerListData } from "@/types/manager";
 
 type CenterManagerListResponse = {
-  data: ICenterManagerData[];
+  data: ICenterManagerListData;
 } & IResponseDefault;
 
 /**
  * 센터 관리자 목록 조회 API
  * @returns 센터 관리자 목록 조회 응답
  */
-export const getCenterManagers = async () => {
+export const getCenterManagers = async (search: string) => {
+  const params: Record<string, string | number> = {};
+  if (search?.trim() && search !== "") {
+    params.search = search;
+  }
   const { data: responseData } =
-    await customAxios.get<CenterManagerListResponse>(`/centers/managers`);
+    await customAxios.get<CenterManagerListResponse>(`/centers/managers`, {
+      params,
+    });
   return responseData.data;
 };
