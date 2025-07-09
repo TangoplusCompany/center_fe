171f881e0b60b1b1fbc01392b68204c56d16161b
SeongYoon <namo4862@gmail.com>
2025-07-09 15:14:31 +0900

    refactor: _components 에서 @/components로 위치 수정 및 schema 분리

---
+++ src/schemas/centerSchema.ts
@@ -0,0 +1,24 @@
+import * as z from "zod";
+
+export const centerEditSchema = z.object({
+  centerName: z
+    .string()
+    .min(1, { message: "센터 이름을 입력해주세요." })
+    .regex(/^[가-힣a-zA-Z0-9\s-]+$/, {
+      message: "한글, 영어, 숫자, 띄어쓰기, 하이픈(-)만 입력해주세요.",
+    }),
+  centerAddress: z
+    .string()
+    .min(1, { message: "센터 주소를 입력해주세요." })
+    .regex(/^[가-힣a-zA-Z0-9\s-]+$/, {
+      message: "한글, 영어, 숫자, 띄어쓰기, 하이픈(-)만 입력해주세요.",
+    }),
+  centerAddressDetail: z
+    .string()
+    .regex(/^[가-힣a-zA-Z0-9\s-]+$/, {
+      message: "한글, 영어, 숫자, 띄어쓰기, 하이픈(-)만 입력해주세요.",
+    })
+    .optional(),
+});
+
+export type ICenterEditForm = z.infer<typeof centerEditSchema>;
